caeaf7711ece6bec4fbc2763c399b607b1fb584e
Dzaldy <dzaldysaputro@gmail.com>
2022-12-19 21:33:45 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,19 +1,3 @@
-/**
- * URL aplikasi client berada pada:
- * http://notesapp-v1.dicodingacademy.com/
- * ----------------------------------------------
- * LISENSI DAN SUMBER KODE DIMILIKI OLEH DICODING
- * ----------------------------------------------
- * Untuk lengkapnya kunjungi kelas 'Belajar Membuat Aplikasi Back-End untuk Pemula'.
- * 
- * Sebelum itu ubahlah 'Allow invalid certificates for resources loaded from localhost.'
- * dan 'Allow invalid certificates for resources loaded from localhost.' menjadi enabled di chrome://flags
- * ----------------------------------------------
- * Untuk 'Allow invalid certificates for resources loaded from localhost.' tambahkan tautan berikut dalam box:
- * http://localhost,
- * http://localhost:5000,
- * http://notesapp-v1.dicodingacademy.com
- */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
